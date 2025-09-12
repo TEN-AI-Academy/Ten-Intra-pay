34b56be77118f6342c68e80005a4b92ce84ea4fa
ShauryaBhartiya <iamshauryakgupta@gmail.com>
2025-09-12 17:55:57 +0530

    created home file and folder

---
+++ frontend/src/components/Home/section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const section = () => {
+  return (
+    <div>
+      <h2>Section Title</h2>
+    </div>
+  )
+}
+
+export default section
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Section from '../components/Home/section'
+const Home = () => {
+  return (
+    <div>
+      <Section />
+    </div>
+  )
+}
+
+export default Home
