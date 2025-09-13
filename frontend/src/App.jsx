af03769f8d7e318047b3ca0b20522e3d3654dec8
ManikantaReddy9 <manikantareddymca@gmail.com>
2025-09-13 18:17:25 +0530

    Installed bootstrap and designed testimonals sections in home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import Home from './pages/Home'
-import About from "./pages/About"
-import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
-import './App.css'
-
-function App() {
- 
-  return (
-    <>
-     <Router>
-       <Routes>
-        <Route path="/about" element={<About></About>} />
-       </Routes>
-     </Router>
-    </>
-=======
 import React from 'react'
 import Home from './Home'
 
@@ -23,7 +6,6 @@ const App = () => {
     <div>
       <Home />
     </div>
->>>>>>> 51e77c7 (Added bootstrap and designed testimonals)
   )
 }
 
