f32ee345485ac5ba617d29b5f83c744e73398f24
ShauryaBhartiya <iamshauryakgupta@gmail.com>
2025-09-13 20:32:33 +0530

    added to vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import vercel from '@vercel/vite-plugin-vercel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss(), vercel()]
 })
